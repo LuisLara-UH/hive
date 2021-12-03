7512f2b7a771ebeeae6a4fd8c0c2d6d7f955626c
Nadia <nadia.glez99@gmail.com>
2021-12-03 17:27:17 -0500

    utils: move methods added

--- utils.pl
+++ utils.pl
@@ -1,15 +1,30 @@
 :- [piece].
 
-same_color_piece_adjacent(Color, Row, Column) :-
+:- op(700, fx, is_adjacent).
+:- op(700, fx, same_position).
+:- op(700, fx, same_color).
+:- op(700, fx, found_same_color_piece_adjacent).
+:- op(700, yfx, has_same_color_adjacent).
+
+
+% piece(type, black or white, Piled, Row, Column)
+found_same_color_piece_adjacent Color, Position :-
     get_pieces Pieces,
-    has_adjacent(position(Row, Column), Pieces).
+    has_same_color_adjacent Color, Position, Pieces.
 
-has_adjacent(Piece_Position, [piece(_, _, _, Row, Column)|Y]) :-
-    is_adjacent(Piece_Position, position(Row, Column));
-    has_adjacent(Piece_Position, Y).
 
-same_position(position(Row, Column), position(Row, Column)).
+has_same_color_adjacent Color1, Piece_Position, [piece(_, Color2, _, Row, Column)|Y] :-
+    is_adjacent Piece_Position, position(Row, Column), same_color Color1, Color2;
+    has_same_color_adjacent(Color1, Piece_Position, Y).
 
-is_adjacent(position(Row1, Column1), Y):-
-    same_position(position(Row1 + 1, Column1), Y);
-    same_position(position(Row1, Column1 + 1), Y).
+
+same_position position(Row, Column), position(Row, Column).
+
+
+is_adjacent position(Row, Column), Y :-
+    same_position(position(Row + 1, Column), Y);
+    same_position(position(Row, Column + 1), Y);
+    same_position(position(Row - 1, Column, Y));
+    same_position(position(Row, Column - 1), Y).
+
+same_color Color, Color.
